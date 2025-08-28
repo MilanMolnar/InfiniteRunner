2404e4bf637fdfca2fc39eb73a16c0304bda427c
MilanMolnar <molnarmilan.cc@gmail.com>
2025-08-28 15:08:19 +0200

    scoreboard addded

--- Build/royalrunBuild.loader.js
+++ Build/royalrunBuild.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 92676562,
+    wasmFileSize: 92810355,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
